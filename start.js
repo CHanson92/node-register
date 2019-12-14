37d0ab49398aa026ecd8c11eecac4ab7ec022744
Chris Hanson <BlondyHanson@gmail.com>
2019-12-14 22:28:31 +0000

    Added mongoose and connected to a local mongodb database

--- start.js
+++ start.js
@@ -1,4 +1,19 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
+
+mongoose.connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+mongoose.Promise = global.Promise;
+mongoose.connection
+    .on('connected', () => {
+        console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+    })
+    .on('error', err => {
+        console.log(`Connection error: ${err.message}`);
+    });
 
 const server = app.listen(3000, () => {
     console.log(`Express is running on port ${server.address().port}`);
