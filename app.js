ed7bbc323dacbfb138d1f35170a6444265fc6798
Chris Hanson <BlondyHanson@gmail.com>
2019-12-16 12:45:44 +0000

    Added scss

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const sassMiddleware = require('node-sass-middleware');
 const routes = require('./routes/index');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(
+    sassMiddleware({
+        /* Options */
+        src: path.join(__dirname, 'public/scss'),
+        dest: path.join(__dirname, 'public'),
+        debug: true,
+        outputStyle: 'compressed',
+        sourceMap: true,
+    })
+);
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
